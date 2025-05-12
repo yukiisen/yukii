0f122dfcb8d0b888b0f5e15980ebe0ba859617b9
yukiisen <ahmad.redhorn@gmail.com>
2025-05-12 01:04:12 +0100

    basics

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import '../app.css';
+    import ScreenEffect from './ScreenEffect.svelte';
+
+	let { children } = $props();
+</script>
+
+<!-- the weird screen effect -->
+<ScreenEffect />
+
+<section class="bg-background w-full h-full absolute">
+    {@render children()}
+</section>
---
+++ src/routes/ScreenEffect.svelte
@@ -0,0 +1,15 @@
+<div class="scanlines"></div>
+
+<style>
+    .scanlines {
+        position: absolute;
+        z-index: 5;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background: repeating-linear-gradient(transparent, transparent 1px, rgba(0, 0, 0, 0.1) 1px, rgba(0, 0, 0, 0.1) 2px);
+        box-shadow: 0 0 40px #0009 inset;
+        pointer-events: none;
+    }
+</style>
